570c41bc34aeaf30746c9d8ce37a3e3fdb25bd3f
Younghae Kim <younghae.kim.6405@gmail.com>
2022-08-21 13:00:29 +0900

    first commit

---
+++ src/myPackage.js
@@ -0,0 +1,20 @@
+// @ts-check
+/**
+ * Initialize the project
+ * @param {object} config
+ * @param {string} config.url
+ * @returns {boolean}
+ */
+
+export function init(config) {
+    return true;
+}
+
+/**
+ * Exit the program
+ * @param {number} code 
+ * @returns {number}
+ */
+export function exit(code) {
+    return code+1;
+}
